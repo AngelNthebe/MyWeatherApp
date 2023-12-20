f32448a640532e78ab8e7b4b74c53585dd91acc1
Angel <angelnthebe00@gmail.com--global user.email>
2023-12-20 19:44:16 +0200

    my 3rd commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const close =document.getElementsByClassName("location-selector")
+
+function closebtn(){
+
+    close.style.display = "none" ;
+   
+}
+//     event.preventDefault();
+//     close.style.display = "none"
+//     console.log(event);
+// });
