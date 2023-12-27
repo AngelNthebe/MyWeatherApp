2b9bc4a8c280dadcc3ea197e49bf9369de398408
Angel <angelnthebe00@gmail.com--global user.email>
2023-12-27 13:47:20 +0200

    java for entered location and basic design

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 
-//closing the popup
-function closebtn(){
-   
-    let closepopup = document.getElementById("pop");
-    closepopup.style.display = "none";
-    console.log(close);
-   
-}
-let _location = document.getElementById("location")
-//get device location
-function  getlocation(){
-if (navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(showPosition());
-}else{
-    _location.innerhtml ="location not found"
-
-}
-}
-function showPosition(position){
-    _location.innerhtml = position.coords.latitude +"<br>"+ position.coords.longitude 
-
-}
+function  weather(){
+     const ApiKey ="c259c38445f4965cbfee848f82b935c8";
+     const city = document.getElementById("city").value
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`)
+        .then (response=>response.json())
+        .then(data => {
+            const icon = data.weather[0].icon;
+            const temperature = data.main.temp;
+            const description = data.weather[0].description;
+            document.getElementById('icon').src = `http://openweathermap.org/img/w/${icon}.png`;
+            document.getElementById('temperature').textContent = `Temperature: ${temperature} K`;
+            document.getElementById('description').textContent = `Description: ${description}`;
+          })
+          .catch(error => console.error('Error fetching weather data:', error));
+      }
+    
