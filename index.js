f49848c5198efb8e77dc695f051502720739946a
Angel <angelnthebe00@gmail.com--global user.email>
2023-12-28 22:43:42 +0200

    from K to degrees C . try to round of to 2 decimals

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 function  weather(){
+    
      const ApiKey ="c259c38445f4965cbfee848f82b935c8";
      const city = document.getElementById("city").value
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`)
@@ -9,7 +10,9 @@ function  weather(){
             const temperature = data.main.temp;
             const description = data.weather[0].description;
             document.getElementById('icon').src = `http://openweathermap.org/img/w/${icon}.png`;
-            document.getElementById('temperature').textContent = `Temperature: ${temperature} `;
+           
+            document.getElementById('temperature').textContent = `Temperature: ${temperature - 273.15} °C `;
+            
             document.getElementById('description').textContent = `Description: ${description}`;
           })
           .catch(error => console.error('Error fetching weather data:', error));
