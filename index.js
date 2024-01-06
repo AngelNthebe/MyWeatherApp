66a44ff1cf67a4e202ec2412b564170a613ebcac
Angel <angelnthebe00@gmail.com--global user.email>
2024-01-06 08:40:01 +0200

    final project

--- index.js
+++ index.js
@@ -1,64 +1,79 @@
-// geting the  current weather  using the city name 
-function  weather(){
-     const ApiKey ="c259c38445f4965cbfee848f82b935c8";
-     const city = document.getElementById("city").value
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}&units=metric`)
-        .then (response=>response.json())
-        .then(data => {
-            const icon = data.weather[0].icon;
-            const temperature = data.main.temp;
-            const description = data.weather[0].description;
-            const mintemp= data.main.temp_min;
-            const maxtemp= data.main.temp_max;
-            const feel= data.main.feels_like;
-
-            document.getElementById('icon').src = `http://openweathermap.org/img/w/${icon}.png`;
+const apiKey = "c259c38445f4965cbfee848f82b935c8"
+const city = document.getElementById("city").value
+// let cityname = document.getElementById("cityname").innerHTML
+//  let currentTemp = document.getElementById("currenttemp").innerHTML
+//  let currentIcon = document.getElementById("currentIcon").src
+  //fetching and storing
+  function getforecast() {
+    const apiKey = 'c259c38445f4965cbfee848f82b935c8';
+    const city = document.getElementById("city").value
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayweather( data) 
+      
            
-            document.getElementById('temperature').textContent = `Temperature: ${temperature } °C `;
-            document.getElementById('min-temp').textContent = `min Temperature: ${mintemp} °C `;
-            document.getElementById('max-temp').textContent = `max Temperature: ${maxtemp } °C `
-            document.getElementById('description').textContent = `Description: ${description}`;
-            document.getElementById('feel').textContent = ` feels like: ${feel} °C `;
-          })
-          .catch(error => console.error('Error fetching weather data:', error));
-      }
-    //   geolocation 
-      document.getElementById("locationbtn").addEventListener('click', () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                const { latitude, longitude } = position.coords;
+      
 
-                getWeatherData(latitude, longitude);
-            });
-        } else {
-            alert('Geolocation is not supported by your browser.');
-        }
-      });
-      //getting the current weather using currentlocation geolocation
-      async function getWeatherData(latitude, longitude) {
-        const apiKey = 'c259c38445f4965cbfee848f82b935c8';
-        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-      
-        try {
-            const response = await fetch(apiUrl);
-            const data = await response.json();
-            displayWeatherData(data);
-        } catch (error) {
-            console.error('Error fetching weather data:', error);
-        }
-      }
-      
-      function displayWeatherData(data) {
-        const icon = data.weather[0].icon;
-        const temperature = data.main.temp;
-        const description = data.weather[0].description;
-        const mintemp= data.main.temp_min;
-            const maxtemp= data.main.temp_max;
-            const feel= data.main.feels_like;
-        document.getElementById('icon').src = `http://openweathermap.org/img/w/${icon}.png`;
-        document.getElementById('temperature').textContent = `Temperature: ${temperature } °C `;
-        document.getElementById('min-temp').textContent = `min Temperature: ${mintemp} °C `;
-        document.getElementById('max-temp').textContent = `max Temperature: ${maxtemp } °C `;
-        document.getElementById('description').textContent = `Description: ${description}`;
-        document.getElementById('feel').textContent = ` feels like: ${feel} °C `;
-      } 
+        )
+
+        .catch(error => console.error('Error fetching weather data:', error));
+
+}
+//displaying the data
+function displayweather( data){
+
+           let name = data.city.name
+        let currentTemp = data.list[0].main.temp
+          let currentIcon = data.list[0].weather[0].icon
+         let mintemp = data.list[0].main.temp_min
+         let maxtemp = data.list[0].main.temp_max
+           let pressure = data.list[0].main.pressure
+           let humidity = data.list[0].main.humidity
+           let feel = data.list[0].main.feels_like
+           let wind =data.list[0].wind.speed
+           document.getElementById("mintemp").innerHTML =`min: <br>${mintemp}°C`
+           document.getElementById("maxtemp").innerHTML =`max: <br>${maxtemp}°C`
+           document.getElementById("humidity").innerHTML =`humidity: <br>${humidity}%`
+           document.getElementById("feel").innerHTML =`feels like: <br>${feel}°C`
+           document.getElementById("wind").innerHTML =`wind speed: <br>${wind} m/h`
+           document.getElementById("pressure").innerHTML =`pressure: <br>${pressure} hPa`
+          document.getElementById("currenttemp").innerHTML =`${currentTemp} °C`
+          document.getElementById("cityname").innerHTML= `${name}`
+          document.getElementById("currentIcon").src =`https://openweathermap.org/img/w/${currentIcon}.png`;
+          //looping for 5day forecast 
+          for (let i = 0; i < data.list.length; i += 8){
+           console.log(data.list[i])
+           index =data.list.i
+          date = data.list[i].dt_txt
+          day=date.substr(0,11)
+         icon =data.list[i].weather[0].icon
+          description =data.list[i].weather[0].description
+          tempmin =data.list[i].main.temp_min
+          tempmax =data.list[i].main.temp_max
+          let showforecast = document.getElementById("display")
+          showforecast.innerHTML +=`
+          <div id=shows${index} class="flex size ">
+          <p id="today"> ${day}</p>
+          <img id="icons" src ="https://openweathermap.org/img/w/${icon}.png" alt="">
+          <p id="describtion"> ${description}</p>
+          <p id="temp"> ${tempmin} /${tempmax}</p> 
+          </div>
+          `;
+          closepg()        
+}
+} 
+
+// closing the page with inputs and btn
+function closepg(){
+    let popU = document.getElementById("cover")
+    popU.style.display = "none"
+    
+}
+ //show cover page again inoder to search again
+ function openpg(){
+    let popU = document.getElementById("cover")
+    popU.style.display = "flex"
+ }
+ 
+
